a8e19232ea064b8e6639ca8f56a2b2817305af33
linlei <linlei0614@foxmail.com>
2020-05-25 09:48:09 +0800

    three

--- src/stream.js
+++ src/stream.js
@@ -7,7 +7,7 @@ const streamm = () => {
         console.log(e)
     })
     rs.on('end', function () {   // 结束执行
-        console.log('END');
+        console.log('END_14');
     });
     
     rs.on('error', function (err) {  // 报错执行
@@ -16,15 +16,15 @@ const streamm = () => {
 
     // 写入流
     const ws = fs.createWriteStream('./txt/text.txt', 'utf-8');
-    ws.write('使用Stream写入文本数据...\n');
+    ws.write('使用Stream写入文本数据...12\n');
     ws.write('END.');
     ws.end() // 写入成功，关闭
     
-    var ws2 = fs.createWriteStream('./txt/output2.txt');
-    ws2.write(new Buffer('使用Stream写入二进制数据...\n', 'utf-8'));
-    ws2.write(new Buffer('END.', 'utf-8'));
-    ws2.end();
+    // var ws2 = fs.createWriteStream('./txt/output2.txt');
+    // ws2.write(new Buffer('使用Stream写入二进制数据...\n', 'utf-8'));
+    // ws2.write(new Buffer('END.', 'utf-8'));
+    // ws2.end();
     
-    rs.pipe(ws2)
+    // rs.pipe(ws2)
 }
 export default streamm;
