ed57558e5cfa4ffad3ed54e260393e5f20a12e65
linlei <linlei0614@foxmail.com>
2020-06-03 15:10:25 +0800

    feat(express-generator): 新增vue打包文件，以及模拟数据

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 运行
 browser -> npm run dev
 node -> npx babel-node src/test.js
+
+vue目录下是打包好的vue项目文件
+cd到vue目录
+运行项目
+npm start
--- src/express/express.js
+++ src/express/express.js
@@ -6,8 +6,47 @@ import path from 'path';
 
 const app = express();
 
+var data = {
+    name: "网站",
+    num: 3,
+    sites: [
+        {
+            name: "Google",
+            info: [
+                "Android",
+                "Google 搜索",
+                "Google 翻译"
+            ]
+        },
+        {
+            name: "Runoob",
+            info: [
+                "菜鸟教程",
+                "菜鸟工具",
+                "菜鸟微信"
+            ]
+        },
+        {
+            name: "Taobao",
+            info: [
+                "淘宝",
+                "网购"
+            ]
+        }
+    ]
+};
+// 允许跨域
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", ' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
 app.get('/', (request, response) => {
-    response.send('Hello world');
+    response.send(data);
 });
 app.get('/git', (request, response) => {
     response.cookie('123123', 'qweqweqwe');
@@ -21,7 +60,7 @@ app.get('/git', (request, response) => {
 // extended：false 表示使用系统模块的querystring来处理（post方法）
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 // array是上传file类型input标签的name名称,number为最多上传个数
-app.use(multer({ dest: '/tmp/'}).array('image', 2));
+app.use(multer({ dest: '/tmp/' }).array('image', 2));
 
 app.post('/process', urlencodedParser, (request, response) => {
     const name = request.body.name;
@@ -46,7 +85,7 @@ app.post('/file', (request, response) => {
         });
     });
 });
-const server = app.listen(8081, () => {
+const server = app.listen(8082, () => {
     const host = server.address().address;
     const port = server.address().port;
     console.log(`应用访问地址为http://127.0.0.1:${port}`);
